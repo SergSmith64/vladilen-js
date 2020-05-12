a4addd162a6044eaa9d0b90b648262d2b85b8fcc
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-13 02:37:23 +0400

    fix day...

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 console.log("HeLLo PeopLe!");
+
+var ddd = 5;
+
+var ddd = "hello!";
+
+let fff = 89;
+
+// let fff = 8;
+fff = 8;
+
+const ggg = 666;
+
+
+
+console.log("__ddd__= ", ddd);
+console.log("__fff__= ", fff);
+console.log("__ggg__= ", ggg);
+
