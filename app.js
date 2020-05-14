a3b3a5cbc84811a234cf32901cf8383bead5e4cb
SergSmith64 <sergsmith2008@yandex.ru>
2020-05-14 12:44:12 +0400

    "feat: add ternary operator"

--- app.js
+++ app.js
@@ -11,5 +11,30 @@ console.log("_имя_", firstName, "  __возраст_ = ", age.toString());
 console.log("_имя_" + firstName + "  __возраст_ = " + age);
 console.log("__ggg__= ", ggg);
 
+// _________ ввод данных ______________
 const lastName = prompt("Введите фамилию");
 console.log(firstName + " " + lastName);
+
+const num1 = 42;    // number
+const num2 = "42";  // string
+console.log(num1 == num2);
+console.log(num1 === num2);
+
+const isReady = false;
+if (isReady === true) {
+    console.log("Все готово!");
+}
+// ___ короткий вариант написания ___
+if (isReady) {
+    console.log("Все готово!");
+} else {
+    console.log("Все НЕ готово!");
+}
+
+// __ еще более короткий вариант - тернарный оператор ___
+isReady ? console.log("Все___готово!") : console.log("Все___НЕ___готово!");
+
+// __ БУЛЕВАЯ ЛОГИКА __
+
+
+
